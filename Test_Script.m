91db5275a1cbe06bc28c8f7552a58487a289cf47
nepacheco <nepacheco@wpi.edu>
2022-01-31 17:04:10 -0500

    Added a test script

---
+++ Test_Script.m
@@ -0,0 +1,28 @@
+% clc; clear; close all;
+img = imread("Test_images\Fiducial_example.png");
+
+[mm_per_pixel, fiducial_pos, Twinp] = calibrate_camera(img,'Style',...
+    'rectangle','World_Rotation', [1 0 0; 0 -1 0; 0 0 -1]);
+%%
+
+[Tr_in_c, Tb_in_c] = locate_robot(img,'Robot_rotation',rotz(90)*rotx(90),...
+    'mm_per_pix',mm_per_pixel);
+
+%% Get Fiducials in world frame
+% fiducial_pos_c = (fiducial_pos.*mm_per_pixel)';
+% fiducial_pos_w = (Tc_in_w*[fiducial_pos_c;zeros(1,4);ones(1,4)]);
+% 
+% fiducial_pos_r = Tr_in_w*fiducial_pos_w;
+fiducial_pos_r_2 = inv(Tr_in_c)*[fiducial_pos';zeros(1,4);ones(1,4)]
+
+%% TESTING
+known_fiducial_pos = [20 20; 70 35; 160 25; 50 100; 105 85; 140 110];
+known_fiducial_pos = known_fiducial_pos + [2.5 2.5];
+
+robot_0_pose = [rotz(0) [30+cosd(0)*16 + sind(0)*1.1; 65+sind(0)*16 + cosd(0)*1.1; 0]; zeros(1,3) 1]*[rotz(0)*rotx(0) zeros(3,1); zeros(1,3) 1];
+robot_15_pose = [rotz(15) [60+cosd(15)*16 + sind(15)*1.1; 65+sind(15)*16 + cosd(15)*1.1; 0]; zeros(1,3) 1]*[rotz(90)*rotx(90) zeros(3,1); zeros(1,3) 1];
+robot_90_pose = [rotz(90) [90+cosd(90)*16 + sind(90)*1.1; 65+sind(90)*16 + cosd(90)*1.1; 0]; zeros(1,3) 1]*[rotz(90)*rotx(90) zeros(3,1); zeros(1,3) 1];
+
+known_fiducial_in_0robot = inv(robot_0_pose)*[known_fiducial_pos';zeros(1,6);ones(1,6)];
+known_fiducial_in_15robot = inv(robot_15_pose)*[known_fiducial_pos';zeros(1,6);ones(1,6)];
+known_fiducial_in_90robot = inv(robot_90_pose)*[known_fiducial_pos';zeros(1,6);ones(1,6)];
